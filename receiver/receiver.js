39cc3d3973312158f4dff1c29b68f38f7a606359
Simon Prickett <simon.prickett@crate.io>
2024-06-14 17:06:57 +0100

    Selectively logs messages, uses debug flag.

--- receiver/receiver.js
+++ receiver/receiver.js
@@ -4,6 +4,7 @@ import * as sbs1 from 'sbs1';
 
 dotenv.config();
 
+const DEBUG_MODE = process.env.DEBUG_MODE === '1';
 const SBS_HOST = process.env.SBS_HOST || '127.0.0.1';
 const SBS_PORT = process.env.SBS_PORT || 30003;
 
@@ -13,5 +14,32 @@ const sbs1Client = sbs1.createClient({
 });
 
 sbs1Client.on('message', async(msg) => {
-  console.log(msg);
+  if (DEBUG_MODE) { console.log(msg); }
+
+  const msgData = {
+    hex_ident: msg.hex_ident,
+    last_updated: Date.now()
+  };
+
+  if (msg.lat && msg.lon) {
+    msgData.lat = msg.lat;
+    msgData.lon = msg.lon;
+    msgData.position = `POINT (${msg.lon} ${msg.lat})`;
+  }
+
+  if (msg.altitude) {
+    msgData.altitude = msg.altitude;
+  }
+
+  if (msg.callsign) {
+    msgData.callsign = msg.callsign.trim();
+  }
+
+  if (msg.squawk) {
+    msgData.squawk = msg.squawk.trim();
+  }
+
+  if (Object.keys(msgData).length > 2) {
+    console.log(msgData);
+  }
 });
