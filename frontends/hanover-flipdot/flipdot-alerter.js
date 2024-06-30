3ada90bfb5358fe58e648fec3dc30856655dd7bd
Simon Prickett <simon.prickett@crate.io>
2024-06-30 19:43:47 +0100

    Added work in progress flipdot sign front end.

---
+++ frontends/hanover-flipdot/flipdot-alerter.js
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv';
+import mqtt from 'mqtt';
+
+dotenv.config();
+
+const DEBUG_MODE = process.env.DEBUG_MODE === '1';
+
+const mqttClient = mqtt.connect(`${process.env.MQTT_PROTOCOL}://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`, {
+  username: process.env.MQTT_USER,
+  password: process.env.MQTT_PASSWORD
+});
+
+console.log(`Connected as ${process.env.MQTT_USER} to MQTT broker at ${process.env.MQTT_HOST}:${process.env.MQTT_PORT}.`);
