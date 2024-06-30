d559403880532dd9ff2903334c068c4f433ab53d
Simon Prickett <simon.prickett@crate.io>
2024-06-30 20:00:21 +0100

    Receives and logs messages.

--- frontends/hanover-flipdot/flipdot-alerter.js
+++ frontends/hanover-flipdot/flipdot-alerter.js
@@ -11,3 +11,9 @@ const mqttClient = mqtt.connect(`${process.env.MQTT_PROTOCOL}://${process.env.MQ
 });
 
 console.log(`Connected as ${process.env.MQTT_USER} to MQTT broker at ${process.env.MQTT_HOST}:${process.env.MQTT_PORT}.`);
+
+mqttClient.subscribe(process.env.MQTT_TOPIC);
+mqttClient.on('message', (topic, message) => {
+  const flightDetails = JSON.parse(message.toString());
+  console.log(flightDetails);
+});
