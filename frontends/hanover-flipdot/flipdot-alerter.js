faf28dd6cb2e6b53fd329764979a311c65641070
Simon Prickett <simon.prickett@crate.io>
2024-06-30 21:39:17 +0100

    Added flipdot items for front end.

--- frontends/hanover-flipdot/flipdot-alerter.js
+++ frontends/hanover-flipdot/flipdot-alerter.js
@@ -1,9 +1,29 @@
+import FlipDot from 'flipdot-display';
 import * as dotenv from 'dotenv';
 import mqtt from 'mqtt';
+import { Mutex} from 'async-mutex';
 
 dotenv.config();
 
 const DEBUG_MODE = process.env.DEBUG_MODE === '1';
+
+async function sleep(millis) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millis);
+  });
+}
+function calculatePixelWidth(msg) {
+  let pixelCount = 0;
+
+  for (const char of msg) {
+    // Regular chars 9 pixels wide, a space is 1.
+    pixelCount += (char === ' ' ? 1 : 9);
+  }
+
+  return pixelCount;
+}
+
+const flippy = new FlipDot(process.env.SIGN_DEVICE, process.env.SIGN_ADDRESS, process.env.SIGN_ROWS, process.env.SIGN_COLS);
 
 const mqttClient = mqtt.connect(`${process.env.MQTT_PROTOCOL}://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`, {
   username: process.env.MQTT_USER,
@@ -12,8 +32,52 @@ const mqttClient = mqtt.connect(`${process.env.MQTT_PROTOCOL}://${process.env.MQ
 
 console.log(`Connected as ${process.env.MQTT_USER} to MQTT broker at ${process.env.MQTT_HOST}:${process.env.MQTT_PORT}.`);
 
-mqttClient.subscribe(process.env.MQTT_TOPIC);
-mqttClient.on('message', (topic, message) => {
+// The sign takes a long time to update, so let's use a Mutex in case subsequent
+// flight details arrive while we are showing one...
+// https://www.npmjs.com/package/async-mutex
+const signMutex = new Mutex();
+
+async function displayData(lines) {
+  await signMutex.runExclusive(async () => {
+    for (let n = 0; n < SIGN_REPEATS; n++) {
+      for (const line of lines) {
+        const xOffset = Math.floor((SIGN_COLS - calculatePixelWidth(line)) / 2);
+        flippy.writeText(line, { font: 'Banner3' }, [0, xOffset], false, true);
+        flippy.send();
+        await sleep(process.env.SIGN_FLIP_INTERVAL);
+      }
+
+      flippy.fill(0xFF);
+      if (n < process.env.SIGN_REPEATS) {
+        await sleep(process.env.SIGN_FLIP_INTERVAL);
+      }
+    }
+  });
+}
+
+flippy.on('error', (err) => {
+  console.error('Flippy Error:');
+  console.error(err);
+});
+
+flippy.once('open', () => {
+  console.log(`Connected to flip dot device at ${SIGN_DEVICE}.`);
+  flippy.fill(0xFF);
+});
+
+mqttClient.on('message', async (topic, message) => {
   const flightDetails = JSON.parse(message.toString());
   console.log(flightDetails);
+
+  const dataToDisplay = [
+    flightDetails.flightNumber,
+    `${flightDetails.origin} - ${flightDetails.destination}`,
+    flightDetails.aircraftType,
+    flightDetails.registration,
+    `${flightDetails.altitude}FT`
+  ];
+
+  await displayData(dataToDisplay);
 });
+
+mqttClient.subscribe(process.env.MQTT_TOPIC);
